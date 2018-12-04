7af3ae6ae74de0ada2814eaeb26ae5dab68fc7aa
yana.maletskaya <yana.maletskaya@mobileiron.com>
2018-12-04 20:34:26 +0200

    second

--- src/main/java/MyName.java
+++ src/main/java/MyName.java
@@ -1,28 +1,43 @@
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-
-import java.io.IOException;
 import java.util.Scanner;
 
 public class MyName {
 
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-
-
-    }
+    private static String name;
+    private static String age;
 
     private static int getAge(Scanner scanner) {
         try {
-            System.out.println("Put your age");
-            String age = scanner.nextLine();
+            System.out.println("Enter your age");
+            age = scanner.nextLine();
             return Integer.parseInt(age);
         } catch (Exception e) {
             return getAge(scanner);
-
         }
     }
 
+    private static String getName(Scanner scanner) {
+//        try {
+            System.out.println("Enter your name");
+            name = scanner.nextLine();
+            if (name.matches("[A-Za-Z]")) {
+                return name;
+            } else {
+                System.out.println("It's not a character");
+            }
 
+//        } catch (Exception e) {
+//            return getName(scanner);
+        return name;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int age = getAge(scanner);
+        name = getName(scanner);
+
+        System.out.println("My name is" + " " + name + " " + "and I amd " + age + " " + "years old");
+
+    }
 }
